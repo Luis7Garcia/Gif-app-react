8355d1521decee3e7401e52024d2ad321ca906ca
Luis7 <luishgarcia_97@hotmail.com>
2024-03-21 17:28:20 -0500

    Crear aplicacion gif

---
+++ src/GifAplication.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+
+
+export const GifAplication = () => {
+
+    const [categories, setCategories] = useState([ 'One punch' ]);
+
+    const onAddCategory = (newCategory) =>{
+
+        if( categories.includes(newCategory) ) return;
+       
+        setCategories([newCategory, ...categories ]);
+    }
+
+  return (
+    <>
+        {/*Titulo */}
+        <h1>GifAplication</h1>
+
+        <AddCategory
+              // setCategories={setCategories}
+              onNewCategory={(value) => onAddCategory(value)}
+        />
+
+           
+            {
+                categories.map( ( category ) => (
+                <GifGrid key={ category }
+                category={ category }/>
+                
+            ))
+        }
+       
+    </>
+  )
+}
+
+
